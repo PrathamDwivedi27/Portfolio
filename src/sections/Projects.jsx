78e87fc9586f1f1070efe4b5260e39a1e2b38e37
PrathamDwivedi27 <prathamd549@gmail.com>
2024-12-17 10:35:34 +0530

    First version is done

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -94,6 +94,7 @@ const Projects = () => {
 
         <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
           <Canvas>
+            <div>hello there</div>
             <ambientLight intensity={Math.PI} />
             <directionalLight position={[10, 10, 5]} />
             <Center>
